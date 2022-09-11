70fe3f96b7151d1271459061f54289292feec020
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-09-10 21:03:08 -0300

    Fix

--- src/HenryDM/ToolsUI/Main.php
+++ src/HenryDM/ToolsUI/Main.php
@@ -1,56 +1,55 @@
 <?php
 
-namespace HenryDM\ToolsUI;
+namespace HenryDM\CustomPVP;
 
-# Plugin Forms Libs 
-use HenryDM\ToolsUI\Form\SimpleForm;
-use HenryDM\ToolsUI\Form\CustomForm;
-use HenryDM\ToolsUU\Form\ModalForm;
-
-# Pocketmine Libs
-use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
 
-# Plugin Events Libs 
-use HenryDM\ToolsUI\Events\Join\JoinMessage;
-use HenryDM\ToolsUI\Events\Join\JoinSound;
-use HenryDM\ToolsUI\Events\Quit\QuitSound;
-use HenryDM\ToolsUI\Events\Quit\QuitClear;
+use HenryDM\CustomPVP\Events\Join\JoinMessage;
+use HenryDM\CustomPVP\Events\Join\JoinSound;
+
+use HenryDM\CustomPVP\Events\Quit\QuitClear;
+use HenryDM\CustomPVP\Events\Quit\QuitSound;
 
 class Main extends PluginBase implements Listener {
 
-      public function onEnable() : void {
-        $this->saveDefaultConfig();
-        if($this->getConfig()->get("Config-Version") < "1.0.0") {
-          $this->getLogger()->warning("Your config file is outdate, Please consider update!");
-          rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config_outdate.yml");
-        }
+    /*** @var Main|null */
+    private static Main|null $instance;
+
+    /*** @var Config */
+    public Config $cfg;
+
+    public function onEnable() : void {
+        $this->saveResource("config.yml");
+        $this->cfg = $this->getConfig(); 
+    }
+
+    public function onLoad() : void {
+      self::$instance = $this;
+      if($this->getConfig()->get("config-version") < 1.0.0) {
+        $this->getLogger()->warning("Your configuration is outdate! Please consider update.");
+        rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config_outdate.yml");
       }
+    }    
+    
+    $events = [
+      JoinMessage::class,
+      JoinSound::class,
+      QuitSound::class,
+      QuitClear::class
+    
+    ];
+    
+    foreach($events as $ev) {
+      $this->getServer()->getPluginManager()->registerEvents(new $ev($this), $this);
+    }
 
-        $events = [
-          JoinMessage::class,
-          JoinSound::class,
-          QuitSound::class,
-          QuitClear::class
-        ];
+    public function getInstance() : Main {
+      return self::$instance;
+    }
 
-        foreach($events as $ev) {
-            $this->getServer()->getPluginManager()->registerEvents(new $ev($this), $this);
-        }
-        
-        public function onDisable() : void {
-          $this->saveDefaultConfig();
-        }
-        
-        public function onLoad() : void {
-          self::$instance = $this;
-        }
-      
-        public function getInstance() : Main {
-          return self::$instance;
-        }
-  
-        public function getMainConfig() : Config {
-          return $this->cfg;
-        }
-} 
+    public function getMainConfig() : Config {
+      return $this->cfg;
+    }
+}
