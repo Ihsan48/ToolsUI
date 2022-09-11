680d36e228b4fe661c59226a374896bbaa76eddb
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-09-10 21:09:29 -0300

    Fix all

--- src/HenryDM/ToolsUI/Main.php
+++ src/HenryDM/ToolsUI/Main.php
@@ -1,55 +1,49 @@
 <?php
 
-namespace HenryDM\CustomPVP;
+namespace HenryDM\ToolsUI;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 
-use HenryDM\CustomPVP\Events\Join\JoinMessage;
-use HenryDM\CustomPVP\Events\Join\JoinSound;
+use HenryDM\BlockPerms\Events\Join\JoinMessage;
+use HenryDM\BlockPerms\Events\Join\JoinSound;
 
-use HenryDM\CustomPVP\Events\Quit\QuitClear;
-use HenryDM\CustomPVP\Events\Quit\QuitSound;
+use HenryDM\BlockPerms\Events\Quit\QuitClear;
+use HenryDM\BlockPerms\Events\Quit\QuitSound;
 
 class Main extends PluginBase implements Listener {
 
-    /*** @var Main|null */
-    private static Main|null $instance;
+    /*** @var Main */
+    private static Main $instance;
 
     /*** @var Config */
     public Config $cfg;
 
     public function onEnable() : void {
-        $this->saveResource("config.yml");
-        $this->cfg = $this->getConfig(); 
+        $this->saveDefaultConfig();
+        $this->cfg = $this->getConfig();
+
+        $events = [
+            JoinMessage::class,
+            JoinSound::class,
+            QuitClear::class,
+            QuitSound::class
+        ];
+        foreach($events as $e) {
+            $this->getServer()->getPluginManager()->registerEvents(new $e($this), $this);
+        }
     }
 
     public function onLoad() : void {
-      self::$instance = $this;
-      if($this->getConfig()->get("config-version") < "1.0.0") {
-        $this->getLogger()->warning("Your configuration is outdate! Please consider update.");
-        rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config_outdate.yml");
-      }
-    }    
-    
-    $events = [
-      JoinMessage::class,
-      JoinSound::class,
-      QuitSound::class,
-      QuitClear::class
-    
-    ];
-    
-    foreach($events as $ev) {
-      $this->getServer()->getPluginManager()->registerEvents(new $ev($this), $this);
+        self::$instance = $this;
     }
 
-    public function getInstance() : Main {
-      return self::$instance;
+    public static function getInstance() : Main {
+        return self::$instance;
     }
 
     public function getMainConfig() : Config {
-      return $this->cfg;
+        return $this->cfg;
     }
 }
